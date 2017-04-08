6e990ec229d40e3a7f647dc957782ca71cbae357
Sameeullah Babar <Sameeullah Babar>
2017-04-08 15:53:58 -0500

    peanut butter jelly

---
+++ cmake-build-debug/CMakeFiles/InterpreterA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InterpreterA.dir/main.cpp.obj"
+  "InterpreterA.pdb"
+  "InterpreterA.exe"
+  "InterpreterA.exe.manifest"
+  "libInterpreterA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InterpreterA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/InterpreterA.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for InterpreterA.
+# This may be replaced when dependencies are built.
